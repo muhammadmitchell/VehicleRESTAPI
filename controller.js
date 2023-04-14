bf76ff5d1df18da23f1ff54763a0b22e323a0512
muhammadmitchell <muhammadmitchell06@gmail.com>
2023-04-15 01:58:45 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -10,11 +10,25 @@ exports.index = function(req, res){
 
 //menampilkan semua data vehicle
 exports.tampilsemuavehicle = function(req, res){
-    connection.query('SELECT * FROM vehicle_brand', function(error, rows, fileds){
-        if(error){
-            connection.log(error);
-        } else {
-            response.ok(rows, res)
+    connection.query('SELECT * FROM vehicle_brand', 
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            } else {
+                response.ok(rows, res)
         }
     });
+};
+
+//menampilkan semua data vehicle berdasarkan vehicle
+exports.tampilberdasarkanid = function(req, res){
+    let id = req.params.id;
+    connection.query("SELECT * FROM vehicle_brand WHERE id = ?", [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
 };
