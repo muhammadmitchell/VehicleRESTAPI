f1a95872607f17ef3bdfd398beb12fac4d3d244b
muhammadmitchell <muhammadmitchell06@gmail.com>
2023-04-15 02:24:06 +0700

    berhasil tambah data

--- controller.js
+++ controller.js
@@ -4,31 +4,46 @@ var response = require('./res');
 var connection = require('./koneksi');
 const conn = require('./koneksi');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok("Vehicle REST API berjalan!", res)
 };
 
 //menampilkan semua data vehicle
-exports.tampilsemuavehicle = function(req, res){
-    connection.query('SELECT * FROM vehicle_brand', 
-        function(error, rows, fields){
-            if(error){
+exports.tampilsemuavehicle = function (req, res) {
+    connection.query('SELECT * FROM vehicle_brand',
+        function (error, rows, fields) {
+            if (error) {
                 console.log(error);
             } else {
                 response.ok(rows, res)
-        }
-    });
+            }
+        });
 };
 
 //menampilkan semua data vehicle berdasarkan vehicle
-exports.tampilberdasarkanid = function(req, res){
+exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query("SELECT * FROM vehicle_brand WHERE id = ?", [id],
-        function(error, rows, fields){
-            if(error){
+        function (error, rows, fields) {
+            if (error) {
                 console.log(error);
             } else {
                 response.ok(rows, res);
+            }
+        });
+};
+
+//menambahkan data vehicle 
+exports.tambahVehicle = function (req, res) {
+    var name = req.body.name;
+
+    connection.query("INSERT INTO vehicle_brand (name) VALUES(?)",
+        [name],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Menambahkan Data!", res)
             }
         });
 };
