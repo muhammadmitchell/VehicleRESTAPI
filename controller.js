ed7d1a51c925ddb2f2a4e2f678d82d4641ab3c8e
muhammadmitchell <muhammadmitchell06@gmail.com>
2023-04-15 01:50:43 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -2,7 +2,19 @@
 
 var response = require('./res');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Vehicle REST API berjalan!")
+    response.ok("Vehicle REST API berjalan!", res)
+};
+
+//menampilkan semua data vehicle
+exports.tampilsemuavehicle = function(req, res){
+    connection.query('SELECT * FROM vehicle_brand', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
