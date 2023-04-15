cc093d70bb8f2c2c39a6df7807a969679047e2a8
muhammadmitchell <muhammadmitchell06@gmail.com>
2023-04-15 21:20:03 +0700

    login user

--- middleware/index.js
+++ middleware/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 
+// daftarkan menu login
+router.post('/api/v1/login', auth.login);
+
 
 module.exports = router;
